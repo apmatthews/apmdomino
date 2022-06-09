36a49fecff0b0cd146982ea7aa9c0415399819f0
Andrew Matthews <andrew.matthews@wpengine.com>
2022-06-09 21:45:37 +0100

    initial commit

---
+++ wp-content/plugins/my-cicd-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: My CI/CD Plugin
+ * Plugin URI: https://wpengine.com/
+ * Description: Test plugin for CI/CD things.
+ * Author: Andrew Matthews
+ * Author URI: https://github.com/apmatthews
+ * Text Domain: my-cicd-plugin
+ * Domain Path: /languages
+ * Version: 0.0.1
+ *
+ * @package My_CICD_Plugin
+ */
+ 
+ register_post_type('cicd');
