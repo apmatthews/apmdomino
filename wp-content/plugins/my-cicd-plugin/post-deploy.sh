fe4ae470e646dacb30e046d1a1aa53a07a727a2d
Andrew Matthews <andrew.matthews@wpengine.com>
2022-07-06 18:49:44 -0400

    adjust deploy

--- wp-content/plugins/my-cicd-plugin/post-deploy.sh
+++ wp-content/plugins/my-cicd-plugin/post-deploy.sh
@@ -2,4 +2,4 @@
 TEST_PLUGIN_VERSION=$(wp plugin get my-cicd-plugin | sed -n "/version/p" | cut -f2)
 echo "::set-output name=version::$TEST_PLUGIN_VERSION"
 # Restore from backup
-rm wp-content/plugins/my-cicd-plugin && mv wp-content/plugins/tmp/my-cicd-plugin wp-content/plugins/
+rm -rf wp-content/plugins/my-cicd-plugin && mv wp-content/plugins/tmp/my-cicd-plugin wp-content/plugins/
