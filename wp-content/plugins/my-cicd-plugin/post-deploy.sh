bcfa241e0ebd94c7ab272e86f0f0bd6c3f17cc32
Andrew Matthews <andrew.matthews@wpengine.com>
2022-07-06 18:48:21 -0400

    adjust deploy

--- wp-content/plugins/my-cicd-plugin/post-deploy.sh
+++ wp-content/plugins/my-cicd-plugin/post-deploy.sh
@@ -1,5 +1,5 @@
 # Try to set this as action output
-TEST_PLUGIN_VERSION=$(wp plugin get my-cicd-plugin | sed -n "/version/p" | cut -d$'\t' -f2)
+TEST_PLUGIN_VERSION=$(wp plugin get my-cicd-plugin | sed -n "/version/p" | cut -f2)
 echo "::set-output name=version::$TEST_PLUGIN_VERSION"
 # Restore from backup
 rm wp-content/plugins/my-cicd-plugin && mv wp-content/plugins/tmp/my-cicd-plugin wp-content/plugins/
