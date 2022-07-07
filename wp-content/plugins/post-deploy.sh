65bfab9b5626720d7d25d9d8def704f673b22669
Andrew Matthews <andrew.matthews@wpengine.com>
2022-07-07 12:24:12 -0400

    simplify jobs

--- wp-content/plugins/post-deploy.sh
+++ wp-content/plugins/post-deploy.sh
@@ -9,7 +9,7 @@ rm -rf wp-content/plugins/my-cicd-plugin && mv /tmp/my-cicd-plugin wp-content/pl
 BEFORE_PLUGIN_VERSION=$(wp plugin get my-cicd-plugin | sed -n "/version/p" | cut -f2)
 echo "::set-output name=before_version::$BEFORE_PLUGIN_VERSION"
 
-if [ "$BEFORE_PLUGIN_VERSION" == "$AFTER_PLUGIN_VERSION" ]; then
+if [[ "$BEFORE_PLUGIN_VERSION" == "$AFTER_PLUGIN_VERSION" ]]; then
     echo "Test plugin was not updated" && exit 1
 else
     echo "Test plugin successfully updated from $BEFORE_PLUGIN_VERSION to $AFTER_PLUGIN_VERSION!"
