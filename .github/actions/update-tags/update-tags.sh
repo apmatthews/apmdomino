9100609074fda635184aad3b1f3502e00042672b
Andrew Matthews <andrew.matthews@wpengine.com>
2022-07-21 13:25:35 -0400

    validate release tag before operating

--- .github/actions/update-tags/update-tags.sh
+++ .github/actions/update-tags/update-tags.sh
@@ -5,33 +5,37 @@ set -e
 : "${RELEASE_TAG_NAME?Release tag name not set.}"
 echo "New release tag is: $RELEASE_TAG_NAME"
 
-createOrUpdateTag() {
-    tagName=$1
+# We will only handle tags >= v1 in the format v{MAJOR}.{MINOR}?.{PATCH}?
+#   capture 1: major number
+#   capture 2: minor number with dot
+#   capture 3: minor number without dot
+#   capture 4: patch number with dot
+#   capture 5: patch number without dot
+VALID_TAG_REGEX="^v([1-9][0-9]*)(\.(0|[1-9][0-9]*)){0,1}(\.(0|[1-9][0-9]*)){0,1}$"
 
-    if [ $(git tag -l $tagName) ]; then
-        echo "Updating tag: $tagName"
-    else
-        echo "Creating tag: $tagName"
-    fi
-
-    git tag -a "$tagName" -m "Release $RELEASE_TAG_NAME" -f
-}
-
-IFS='.' read -a vers <<< "$RELEASE_TAG_NAME"
-
-# TODO: Check tag format
-
-majorTag=${vers[0]}
-minorTag="$majorTag.${vers[1]:-0}"
-patchTag="$minorTag.${vers[2]:-0}"
-
-tags=($majorTag $minorTag $patchTag)
+if [[ "$RELEASE_TAG_NAME" =~ $VALID_TAG_REGEX ]] ; then
+    majorTag="v${BASH_REMATCH[1]}"
+    minorTag="$majorTag.${BASH_REMATCH[3]:-0}"
+    patchTag="$minorTag.${BASH_REMATCH[5]:-0}"
+    tags=("$majorTag" "$minorTag" "$patchTag")
+else
+    echo "Release tag does not match the format \"v{MAJOR}.{MINOR}?.{PATCH}?\". Skipping tag updates."
+    exit 0
+fi
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 for tag in "${tags[@]}"; do
-    createOrUpdateTag "$tag";
-done
+    message="Release $patchTag"
 
-git push --force origin --tags
+    if [ "$(git tag -l "$tag")" ]; then
+        echo "Updating tag: $tag"
+        message="Update to $patchTag"
+    else
+        echo "Creating tag: $tag"
+    fi
+
+    git tag -fa "$tag" -m "$message"
+    git push origin "$tag" --force
+done
