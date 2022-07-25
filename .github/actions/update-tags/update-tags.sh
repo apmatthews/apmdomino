66438ee3efd70fde93183fd3e031b9593f0c39dd
Andrew Matthews <andrew.matthews@wpengine.com>
2022-07-25 17:06:36 -0400

    update script

--- .github/actions/update-tags/update-tags.sh
+++ .github/actions/update-tags/update-tags.sh
@@ -2,19 +2,8 @@
 
 set -e
 
-version=""
-
-if [[ -n "$VERSION" ]]; then
-    version="$VERSION"
-else
-    echo "VERSION not provided. Using latest tag as VERSION..."
-    version="$(git tag --sort=committerdate --points-at HEAD | tail -1)"
-
-    if [[ -z "$version" ]]; then
-        echo "ERROR: Could not determine VERSION or latest tag to update."
-        exit 1
-    fi
-fi
+: "${RELEASE_TAG_NAME?Release tag name not set.}"
+echo "New release tag is: $RELEASE_TAG_NAME"
 
 # We will only handle tags >= v1 in the format v{MAJOR}.{MINOR}?.{PATCH}?
 #   capture 1: major number
@@ -24,11 +13,11 @@ fi
 #   capture 5: patch number without dot
 VALID_TAG_REGEX="^v([1-9][0-9]*)(\.(0|[1-9][0-9]*)){0,1}(\.(0|[1-9][0-9]*)){0,1}$"
 
-if [[ "$version" =~ $VALID_TAG_REGEX ]]; then
+if [[ "$RELEASE_TAG_NAME" =~ $VALID_TAG_REGEX ]] ; then
     majorTag="v${BASH_REMATCH[1]}"
     minorTag="$majorTag.${BASH_REMATCH[3]:-0}"
     patchTag="$minorTag.${BASH_REMATCH[5]:-0}"
-    tags=("$majorTag" "$minorTag" "$patchTag")
+    tagsToUpdate=("$majorTag" "$minorTag")
 else
     echo "Release tag does not match the format \"v{MAJOR}.{MINOR}?.{PATCH}?\". Skipping tag updates."
     exit 0
@@ -37,7 +26,7 @@ fi
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-for tag in "${tags[@]}"; do
+for tag in "${tagsToUpdate[@]}"; do
     message="Release $patchTag"
 
     if [ "$(git tag -l "$tag")" ]; then
